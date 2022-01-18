e2bea3cf11e5dad2d8e6c4a46d632d50bd2800ee
Benis Tambe <will.jenifer@yahoo.com>
2022-01-18 16:50:34 -0500

    commit tree 002c4c867a0fc21b4099808c005bcbd6cb224623

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,17 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+    constructor(props){
+        super();
+        this.state ={
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        
+        return <div>{this.state.secondsLeft == 0 ? "Boom!" : this.state.secondsLeft + " seconds left before I go boom!"} </div> 
+    }
+
+}
